6537014fde8525f96daaca0e3b7561125daffb21
Martincz Gao <martincz@mokeedev.com>
2018-05-13 00:25:50 +0800

    tissot: mokee-ify

--- mokee.mk
+++ mokee.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/xiaomi/tissot/full_tissot.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := mk_tissot
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
